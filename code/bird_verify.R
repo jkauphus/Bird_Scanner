1f0a4f410e029e5a82ab5568822f29bc2d327f7c
Jack Kauphusman <JKauphusman@LOGANSIMPSON.COM>
2023-03-18 09:58:16 -0700

    Bird Scanner, final updates to the workflow tutorial

--- code/bird_verify.R
+++ code/bird_verify.R
@@ -1,22 +1,26 @@
 library(tuneR)
 
 # Write it out once, than create the function or loop
-#audio <- readWave(glue("{to.verify$filepath[1]}"))
-#start_time <- sum(to.verify$start[1]-5)
-#end_time <- sum(to.verify$end[1]+5)
+#audio <- readWave(glue("{verify$filepath[1]}"))
+#start_time <- sum(verify$start[1]-5)
+#end_time <- sum(verify$end[1]+5)
+#start_sample <- as.integer(start_time * audio@samp.rate)
+#end_sample <- as.integer(end_time * audio@samp.rate)
 
 # Extract the segment
-#segment <- window(audio, from = start_time, to = end_time)
+#segment <- audio[start_sample:end_sample]
 
 # Save the segment to a new WAV file
-##writeWave(segment, glue("./checker/{to.verify$scientific_name[1]}_{to.verify$recordingID[1]}"))
+#writeWave(segment, glue("./checker/{to.verify$scientific_name[1]}_{to.verify$recordingID[1]}"))
 #play(segment)
 
 bird_checker <- function(x){
   audio <- readWave(glue("{x$filepath}"))
   start_time <- sum(x$start -5)
   end_time <- sum(x$end +5)
-  segment <- window(audio, from = start_time, to = end_time)
+  start_sample <- as.integer(start_time * audio@samp.rate)
+  end_sample <- as.integer(end_time * audio@samp.rate)
+  segment <- audio[start_sample:end_sample]
   writeWave(segment, glue("./checker/{x$uniqueID}_{x$scientific_name}_{x$recordingID}"))
 }
 
