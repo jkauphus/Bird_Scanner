c36e062cc1e036d900125cdcf93ee33b0b88bc05
Jack Kauphusman <JKauphusman@LOGANSIMPSON.COM>
2023-10-17 11:18:14 -0700

    Small revisions to the setuo_env.R script

--- code/setup_env.R
+++ code/setup_env.R
@@ -1,14 +1,14 @@
 # Set-Up Bird Scanner
 
-Sys.setenv(RETICULATE_PYTHON = "C:/Users/jkauphusman/Anaconda3/envs/pybirdanalyze/python.exe")
+Sys.setenv(RETICULATE_PYTHON = "C:/Users/jkauphusman/Anaconda3/envs/testconda/python.exe")
 library(reticulate)
 library(tidyverse)
 library(openxlsx)
 library(glue)
 
 # Set-up conda env
-use_condaenv(condaenv = "pybirdanalyze", required = TRUE)
-
+use_condaenv(condaenv = "testconda", required = TRUE)
+sys <- import("sys", convert = TRUE)
 # To run the scripts you need to create and activate a conda environment
 
 # Info for when the conda environment does not upload properly
@@ -17,6 +17,10 @@ use_condaenv(condaenv = "pybirdanalyze", required = TRUE)
             #2. conda activate pybirdanalyze
             #3. pip install --upgrade pip
             #4. pip install tensorflow
-            #5. pip install librosa
+            #5. pip install librosa resampy
             #6. pip install numpy==1.20
             #7. pip install birdnetlib
+            #8. pip install pandas
+            #9. pip install xlsxwriter
+            #10. pip install datetime
+            #11. pip install ffmpeg
